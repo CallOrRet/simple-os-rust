21e7aa40d17a3ea6f34d56200803a56b00e244a3
CallOrRet <CallOrRet@outlook.com>
2025-04-26 21:32:03 +0800

    update

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -8,24 +8,16 @@ use simpleos::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+    simpleos::test_panic_handler(info)
 }
 
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    simpleos::test_panic_handler(info);
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
